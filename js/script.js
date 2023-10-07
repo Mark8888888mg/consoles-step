700060169ac0d2b3bc5dfd826bcbf7ee494f8c8d
IT Step~ <itstep@gmail.com>
2023-10-07 12:45:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector(".menu-icon")
+const menuBody = document.querySelector(".header-menu-body")
+
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		iconMenu.classList.toggle("active");
+		menuBody.classList.toggle("active");
+	})
+}
